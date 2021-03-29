956a03cceafcc85323231ed860bab49461e19923
ebisLab <ebaiden@techie.com>
2021-03-29 15:52:49 -0400

    testing

--- client/src/components/__tests__/steps.test.js
+++ client/src/components/__tests__/steps.test.js
@@ -1,24 +1,61 @@
-import {render, screen, cleanup} from '@testing-library/react'
+import {act, render, screen, cleanup, waitFor, fireEvent, findByTestId} from '@testing-library/react'
 import Steps from '../Steps';
+import {mock} from '../../mock';
+import Hero from '../Hero';
+import axios from 'axios';
 
-afterEach(()=>{
-    cleanup()
-})
 
-test('should render Steps component', ()=>{
-    // const steps={
-    //     id: 'd9a439d0-8dbf-4bab-8e08-626f8194a075',
-    //     stepNumber: '1',
-    //     versionContent: [
-    //       {
-    //         title: 'Fill Out a Profile',
-    //         body: 'We only want you to get games and gear that youâ€™ll love, so weâ€™ll ask for your preferences up front.',
-    //         effectiveDate: '2019-05-20T03:04:05.000Z'
-    //       }
-    //     ]
-    //   }
-    render(<Steps />)
+// afterEach(()=>{
+//     cleanup()
+// })
+
+describe("Steps", ()=>{
+
+//before running each test, I'd like to create a mock for the fetch method
+  beforeEach(()=>{
+    
+    global.fetch = jest.fn(()=>Promise.resolve({
+      json: ()=>Promise.resolve(mock)
+    }))
+  })
+
+
+
+test("fetch and render post numbers", async ()=>{
+    render(<Steps/>)
+    await waitFor(()=>
+    mock.forEach(post=>expect(screen.getByText(`0${post.stepNumber}`)).toBeInTheDocument()))
+  })
+
+
+  test('fetch and render "Fill Out a Profile" heading', async ()=>{
+    render(<Steps/>)
+    await waitFor(()=>
+    mock.forEach(post=>expect(screen.getByText(`Fill Out a Profile`)).toBeInTheDocument()))
+  })
+
+
+  test('should render Steps component', async()=>{
+   render(<Steps />)
+    // await act(async ()=>render(<Steps />));
+
     const stepsElement = screen.getByTestId('steps-1');
-    expect(stepsElement).toBeInTheDocument();
+
+    await waitFor(()=>
+    expect(stepsElement).toBeInTheDocument()
+    )
+
+    await waitFor(()=>
     expect(stepsElement).toHaveTextContent('How It works')
+    )
 })
+
+
+test('click on button', async()=>{
+  render(<Hero />);
+  await waitFor(()=>
+  mock.forEach(()=>fireEvent.click(screen.getByText("Get started")))
+  )
+})
+})
+
